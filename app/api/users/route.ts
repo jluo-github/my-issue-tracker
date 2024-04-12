df1d49d79742873e87ee280a0479e0da1f1fae1e
jluo <jluo8@wgu.edu>
2024-04-11 22:02:44 -0700

    update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // get all users
 export async function GET(request: NextRequest) {
-  // @ts-ignore
+  
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(users, { status: 200 });
 }
